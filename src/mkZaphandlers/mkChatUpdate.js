09a453e7c293081e84444b83d4f5dfaf29856a38
Itacir Gabral <itacirgabral@gmail.com>
2021-01-20 11:08:31 -0400

    feat notify webhook

--- src/mkZaphandlers/mkChatUpdate.js
+++ src/mkZaphandlers/mkChatUpdate.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch')
+
 /**
  *  when a chat is updated (new message, updated message, deleted, pinned, presence updated etc)
  * on (event: 'chat-update', listener: (chat: Partial<WAChat> & { jid: string }) => void): this
@@ -5,6 +7,7 @@
 const chatUpdate = ({ shard, redis, connP }) => {
   const logKey = `zap:${shard}:log`
   const newsKey = `zap:${shard}:news`
+  const webhookkey = `zap:${shard}:webhook`
 
   return async (chat) => {
     const json = JSON.stringify({ event: 'chat-update', data: chat })
@@ -12,7 +15,143 @@ const chatUpdate = ({ shard, redis, connP }) => {
     pipeline.lpush(logKey, json)
     pipeline.ltrim(logKey, 0, 99)
     pipeline.publish(newsKey, json)
-    await pipeline.exec()
+    pipeline.get(webhookkey)// 3
+    const result = await pipeline.exec()
+
+    const number = chat.jid.split('@s.whatsapp.net')[0]
+    const webhook = result[3][1]
+
+    // notificar mensagem de texto recebida
+    if (
+      number.indexOf('-') === -1 && // Não é um grupo
+      !chat.presences && // não é uma presença
+      chat.count > 0 // não é ver tudo
+    ) {
+      const message = chat.messages.array[0]
+      const to = shard
+      const from = number
+      const id = message.key.id
+      const msg = message.message.conversation
+      const location = message.message.locationMessage
+      const contact = message.message.contactMessage
+
+      let type
+      if (msg) {
+        type = 'textMessage'
+      } else if (contact) {
+        type = 'contactMessage'
+      } else if (location) {
+        type = 'locationMessage'
+      }
+
+      let jsontosend
+      let vcard
+      switch (type) {
+        case 'textMessage':
+          jsontosend = {
+            type,
+            to,
+            from,
+            id,
+            msg
+          }
+          if (webhook) {
+            fetch(webhook, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(jsontosend)
+            }).catch(() => {})
+          }
+          console.log(JSON.stringify(jsontosend))
+          break
+        case 'locationMessage':
+          jsontosend = {
+            type,
+            to,
+            from,
+            id,
+            description: location.address,
+            latitude: location.degreesLatitude,
+            longitude: location.degreesLongitude
+          }
+          if (webhook) {
+            fetch(webhook, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(jsontosend)
+            }).catch(() => {})
+          }
+          console.log(JSON.stringify(jsontosend))
+          break
+        case 'contactMessage':
+          vcard = vcard = contact.vcard.split('\n').map(el => el.split(':'))
+          jsontosend = {
+            type,
+            to,
+            from,
+            id,
+            name: vcard.find(el => el[0] === 'FN')[1],
+            organization: vcard.find(el => el[0] === 'ORG')[1],
+            whatsapp: vcard.find(el => el[0].slice(0, 3) === 'TEL')[0].split('waid=')[1]
+          }
+          if (webhook) {
+            fetch(webhook, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(jsontosend)
+            }).catch(() => {})
+          }
+          console.log(JSON.stringify(jsontosend))
+          break
+      }
+    }
+
+    /*
+    ** mensagens produzidas pela próprio api não disparam
+    ** message-status-update do tipo sent
+    */
+    if (
+      chat?.messages?.array[0]?.key?.id?.slice(0, 4) === '3EB0' && // produzido por mim
+      number.indexOf('-') === -1 && // Não é um grupo
+      !chat.presences && // não é uma presença
+      typeof chat.count === 'undefined' // Não é mensagem recebida
+    ) {
+      const message = chat.messages.array[0]
+      const to = number
+      const from = shard
+      const id = message.key.id
+
+      if (webhook) {
+        fetch(webhook, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            type: 'sent',
+            timestamp: Date.now(),
+            to,
+            from,
+            id
+          })
+        }).catch(() => {})
+      }
+
+      console.log('SEND TO WEBHOOK')
+      console.log(JSON.stringify({
+        type: 'sent',
+        timestamp: Date.now(),
+        to,
+        from,
+        id
+      }))
+    }
   }
 }
 
--- src/mkZaphandlers/mkMessageStatusUpdate.js
+++ src/mkZaphandlers/mkMessageStatusUpdate.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch')
+
 /**
  * when a message's status is updated (deleted, delivered, read, sent etc.)
  * on (event: 'message-status-update', listener: (message: WAMessageStatusUpdate) => void): this
@@ -5,14 +7,53 @@
 const messageStatusUpdate = ({ shard, redis, connP }) => {
   const logKey = `zap:${shard}:log`
   const newsKey = `zap:${shard}:news`
+  const webhookkey = `zap:${shard}:webhook`
+
+  const types = {
+    2: 'sent',
+    3: 'received',
+    4: 'read'
+  }
 
   return async (message) => {
     const json = JSON.stringify({ event: 'message-status-update', data: message })
     const pipeline = redis.pipeline()
-    pipeline.lpush(logKey, json)
-    pipeline.ltrim(logKey, 0, 99)
-    pipeline.publish(newsKey, json)
-    await pipeline.exec()
+    pipeline.lpush(logKey, json)// 0
+    pipeline.ltrim(logKey, 0, 99)// 1
+    pipeline.publish(newsKey, json)// 2
+    pipeline.get(webhookkey)// 3
+    const result = await pipeline.exec()
+
+    const to = message.to.split('@s.whatsapp.net')[0]
+    if (to.indexOf('-') === -1) {
+      const from = message.from.split('@s.whatsapp.net')[0]
+      const timestamp = new Date(message.timestamp)
+      const type = types[message.type]
+
+      const webhook = result[3][1]
+      message.ids.forEach(id => {
+        const statusUpdate = {
+          type,
+          timestamp: timestamp.getTime(),
+          to,
+          from,
+          id
+        }
+
+        if (webhook) {
+          fetch(webhook, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(statusUpdate)
+          }).catch(() => {})
+        }
+
+        console.log('STATUS UPDATE')
+        console.log(JSON.stringify(statusUpdate))
+      })
+    }
   }
 }
 
